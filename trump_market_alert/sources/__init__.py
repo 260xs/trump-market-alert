e6854d0fc8c98ca9c7206818b1af6cecbac8fc47
260xs <hamad6h@gmail.com>
2026-06-02 19:40:48 +0400

    Delete trump_market_alert directory

--- trump_market_alert/sources/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import Source
-from .rss import RssSource
-from .truth_social import TruthSocialSource
-from .x_api import XApiSource
-from .youtube import YouTubeSource
-
-__all__ = ["Source", "RssSource", "TruthSocialSource", "XApiSource", "YouTubeSource"]
